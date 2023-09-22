dfaf1925114eff3a6c3a8ca57aa29f7d1aef8b07
Abdumanon11 <143066287+Abdumanon11@users.noreply.github.com>
2023-09-22 18:33:36 +0500

    Add files via upload

---
+++ машина/index.js
@@ -0,0 +1,37 @@
+let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+
+let mani = prompt (" Солько увас есть денек ")
+
+    if (mani >= 1000 && mani <= 30000){
+        alert ("welcome good choice " + mani )
+      let car = prompt ("30000, 2000, 11000,2000,14000, 18000, 8000") 
+      if (car >= 1000 && mani <= 30000){
+      alert ("welcome good choice " + car )
+
+      }else {
+        alert ("not enough money")
+    }
+    
+}else {
+    alert ("not enough money")
+
+
+}
+
+
+
+
+
+let students = ['baxtzod', 'xamza', 'islom', 'abdumanon', 'xojik', 'aziz'] 
+
+let even = students.filter((name) => name.length % 3 === 0 )
+console.log(even);
+
+
+
+
+
+
+
+
+
